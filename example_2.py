bd5ec5eb5e323d40c76c91020a9cae60d1de586f
nagireddy <nagireddy6300@gmail.com>
2026-05-05 08:53:07 +0530

    Add files via upload

---
+++ example_2.py
@@ -0,0 +1,9 @@
+name = "naveen"
+age = 23
+salary = 45000.50
+is_employee = True
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_employee))
---
+++ hello.py
@@ -0,0 +1,10 @@
+
+name = "naveen"
+age = 23
+salary = 45000.50
+is_employee = True
+
+print(name)
+print(age)
+print(salary)
+print(is_employee)
